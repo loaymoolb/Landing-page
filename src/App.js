46df158b4709daf2469be4bb01dfd8cea5ec5853
loaymoolb <loya130513@gmail.com>
2022-06-06 19:13:59 +0200

    created file and  folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+
+import { Footer, Blog, Possibility, Features, About, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
 
 const App = () => {
   return (
-    <div></div>
-  )
+    <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>
+    <Brand />
+    <About />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+  );
 }
 
 export default App;
---
+++ src/containers/About/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./About.scss"
+
+const About = () => {
+  return (
+    <div>Oh my god</div>
+  )
+}
+
+export default About
