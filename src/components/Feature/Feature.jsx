ae45a73eb60603b189a42011bd77beb7b9148205
loaymoolb <loya130513@gmail.com>
2022-06-08 11:25:49 +0200

    About section added + changes in navbar

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import "./Feature.scss"
 
 
-const Feature = () => {
+const Feature = ({ title, text }) => {
   return (
-    <div>Feature</div>
+    <div className="features-container__feature">
+      <div className="features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="features-container__feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
   )
 }
 
--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import "./About.scss"
+import { Feature } from "../../components"
 
 const About = () => {
   return (
-    <div>Oh my god</div>
+    <div className="about section__margin" id="about">
+      <div className="about-feature">
+        <Feature
+          title="What is AI-XP"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+         />
+      </div>
+      <div className="about-heading">
+        <h1 className="gradient__text">
+        The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="about-container">
+        <Feature 
+          title="Chatbots"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "
+        />
+       <Feature 
+          title="Knowledgebase"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+        />
+        <Feature 
+          title="Education"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+        />
+      </div>
+    </div>
   )
 }
 
